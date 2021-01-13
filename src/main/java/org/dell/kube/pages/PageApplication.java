954a1fc2e7b5321de8ca3f5952ebb4418f59d5a4
Brixton <aguado@gmail.com>
2021-01-13 13:52:52 +0000

    Lab 11

--- src/main/java/org/dell/kube/pages/PageApplication.java
+++ src/main/java/org/dell/kube/pages/PageApplication.java
@@ -2,9 +2,12 @@ package org.dell.kube.pages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class PageApplication {
 
     public static void main(String[] args) {
