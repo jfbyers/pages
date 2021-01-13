88554f9556d48a10f0b54b557998023de5f2b741
Brixton <aguado@gmail.com>
2021-01-13 09:51:56 +0000

    Lab 9

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -11,3 +11,6 @@ kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/flyway-configmap.yaml
 kubectl apply -f deployment/flyway-job.yaml
+
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
